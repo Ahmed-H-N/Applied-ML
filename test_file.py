696733429aa82f0e2f3b5ad04dd4dac7346163d8
Ahmed-H-N <ahmed.h.abdellah@gmail.com>
2020-02-27 08:53:50 +0200

    add function and test files

---
+++ test_file.py
@@ -0,0 +1,4 @@
+from utility_funcs import inc
+
+def test_inc():
+     assert inc(3) == 4
---
+++ utility_funcs.py
@@ -0,0 +1,8 @@
+# inc.py
+def inc(x):
+    if x < 0:
+        return 0
+    return x + 1
+
+def dec(x):
+     return x - 1
